30bb67c7da6652dbc887b867ef51a5ffa373b991
RC0026850 <RC0026850@CRO-ICF07-09>
2018-01-24 13:56:17 +0000

    Add project files.

---
+++ Forces/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            double FORCE = Convert.ToDouble(textBox1.Text);
+            double ANGLE = Convert.ToDouble(textBox2.Text);
+            double Fx = FORCE * cos(ANGLE);
+            double Fy 
+            Messagebox.show ("Fx = " + Fx)
+        }
+
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+    }
+}
